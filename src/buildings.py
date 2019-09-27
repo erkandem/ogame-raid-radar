7152bfda7a9ec4b9e150444714dc885fa15aa8f6
kan <erkan.demiralay@web.de>
2019-09-27 20:15:50 +0200

    - removed requirement to set values during instantiating (lazy loading pattern)

--- src/buildings.py
+++ src/buildings.py
@@ -12,7 +12,7 @@ class Building:
     universe_speed: Union[float, int]
     cost_factor: Union[float, int]
 
-    def __init__(self, *, level, consumption_factor, production_factor):
+    def __init__(self, *, level=None, consumption_factor=None, production_factor=None):
         self.f_consumption = consumption_factor
         self.production_factor = production_factor
         self.level = level
@@ -28,19 +28,19 @@ class Building:
 
 
 class MetalMine(Building):
-    def __init__(self, level: int):
+    def __init__(self, level: int = None):
         super().__init__(production_factor=30, consumption_factor=10, level=level)
         self.base_production = 210
 
 
 class CrystalMine(Building):
-    def __init__(self, level: int):
+    def __init__(self, level: int=None):
         super().__init__(production_factor=20, consumption_factor=10, level=level)
         self.base_production = 110
 
 
 class DeuteriumSynthesizer(Building):
-    def __init__(self, *, t_max_planet, level, acceleration: int = 1):
+    def __init__(self, *, t_max_planet=None, level=None, acceleration: int = 1):
         super().__init__(production_factor=20, consumption_factor=10, level=level)
         self.acceleration = acceleration
         self.base_cost = OrePrice(metal=225, crystal=75, deuterium=0)
@@ -65,7 +65,7 @@ class DeuteriumSynthesizer(Building):
 
 
 class SolarPlant(Building):
-    def __init__(self, level, etech_level=None, acceleration=1):
+    def __init__(self, level=None, etech_level=None, acceleration=1):
         super().__init__(production_factor=20, consumption_factor=0, level=level)
 
     def get_consumption(self):
@@ -73,7 +73,7 @@ class SolarPlant(Building):
 
 
 class FusionReactor(Building):
-    def __init__(self, level, etech_level=None, acceleration=1):
+    def __init__(self, level=None, etech_level=None, acceleration=1):
         super().__init__(production_factor=30, consumption_factor=10, level=level)
         self.level = level
         self.etech_level = etech_level

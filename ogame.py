b7473c22dc9acfe678d5eeb9f967434d7853b6dc
kan <erkan.demiralay@web.de>
2019-09-27 20:34:06 +0200

    - simplified factory pattern  - added defence, facility, buildings and a wrapper around all of them

--- ogame.py
+++ ogame.py
@@ -1,36 +1,47 @@
 from src import ships
-from properties import ship_properties_mapping, ship_properties
-from properties import defence_properties_mapping, defence_properties
-from properties import defence_properties_mapping, defence_properties
+from properties import ship_properties, ship_properties_mapping
+from properties import defence_properties, defence_properties_mapping
+from properties import facility_properties, facility_properties_mapping
+from properties import building_properties, building_properties_mapping
+from properties import moon_buildings_properties, moon_buildings_properties_mapping
 
 
-def universe_factory():
-    pass
+
+def initializing_logic(props: dict, prop_to_obj_mapping: dict):
+    init_objects = []
+    for obj_name, obj in prop_to_obj_mapping['objects'].items():
+        for property_name, value in props[obj_name].items():
+            attribute_name = prop_to_obj_mapping['attributes'][property_name]
+            obj.__setattr__(attribute_name, value)
+        init_objects.append(obj)
+    return init_objects
 
 
 def ship_factory():
-    initialized_ships = []
-    for ship_name, ship_obj in ship_properties_mapping['objects'].items():
-        for property_name, value in ship_properties[ship_name].items():
-            attribute_name = ship_properties_mapping['attributes'][property_name]
-            ship_obj.__setattr__(attribute_name, value)
-        initialized_ships.append(ship_obj)
-    return initialized_ships
+    return initializing_logic(ship_properties, ship_properties_mapping)
 
 
 def defence_factory():
-    initialized_defence = []
-    for ship_name, defence_obj in defence_properties_mapping['objects'].items():
-        for property_name, value in defence_properties[ship_name].items():
-            attribute_name = defence_properties_mapping['attributes'][property_name]
-            defence_obj.__setattr__(attribute_name, value)
-        initialized_defence.append(defence_obj)
-    return initialized_defence
+    return initializing_logic(defence_properties, defence_properties_mapping)
 
 
 def facilities_factory():
-    pass
+    return initializing_logic(facility_properties, facility_properties_mapping)
 
 
 def buildings_factory():
-    pass
+    return initializing_logic(building_properties, building_properties_mapping)
+
+
+def moon_buildings_factory():
+    return initializing_logic(moon_buildings_properties, moon_buildings_properties_mapping)
+
+
+def universe_factory():
+    return {
+        'moon_buildings': moon_buildings_factory(),
+        'facilities': facilities_factory(),
+        'buildings': buildings_factory(),
+        'ships': ship_factory(),
+        'defences': defence_factory()
+    }

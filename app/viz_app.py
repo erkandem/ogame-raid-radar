9a228a8c68d63509750b977d5e7e2d3b22009524
kan <erkan.dem@pm.me>
2019-12-02 08:28:10 +0100

    - import adjustments

--- app/viz_app.py
+++ app/viz_app.py
@@ -1,11 +1,11 @@
-from src.api.scores_api import HighScoresApi
-from src.api.universe_api import get_janice
+from src.api.scores_api import HighScoresDataApi
+from src.api.universe_api import get_janice_universe
 
 
 def sth_completly_different():
     """ found undocumented. keep until next review Nov2019"""
 
-    janice = get_janice()
-    janice_scores = HighScoresApi(162, 'en', do_init=True)
+    janice = get_janice_universe()
+    janice_scores = HighScoresDataApi(162, 'en', do_init=True)
     inactive = janice.players.query("status == 'i' ")
-    # planet_of_inactivate = janice.universe.join(inactive, )
+    planet_of_inactivate = janice.universe.join(inactive, )

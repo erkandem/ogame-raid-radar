5f1bb89c5c0f836eb9ae391a83c60c0f6149795d
kan <erkan.dem@pm.me>
2020-04-22 17:21:48 +0200

    apply pattern like in other modules

--- src/api/ogame_stats/ogame_stats/universes_api.py
+++ src/api/ogame_stats/ogame_stats/universes_api.py
@@ -1,17 +1,22 @@
 import json
 import requests
+from .utils import ApiBaseClass, nowstr
 
 
-class UniversesData:
+class UniversesData(ApiBaseClass):
 
     def __init__(self):
-        self.data = self.load_data()
+        self.data = self.load_universes()
 
     def _get_universes_url(self) -> str:
         return 'https://lobby.ogame.gameforge.com/api/servers'
 
-    def load_data(self):
+    def _load_data(self, url):
+        """overwrites parents because we receive json instead of xml"""
         response = requests.get(self._get_universes_url())
         json_str = response.content.decode('utf-8')
         return json.loads(json_str)
 
+    def load_universes(self):
+        url = self._get_universes_url()
+        self._load_data_as_df(url)

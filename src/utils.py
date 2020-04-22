17f79df20cffab408541fa98063e52ea9a28c6ff
kan <erkan.dem@pm.me>
2020-04-22 13:40:26 +0200

    add docstring and return type hint and change formatting

--- src/utils.py
+++ src/utils.py
@@ -34,8 +34,17 @@ def calc_distance(
     return distance
 
 
-def calc_flight_time(p1: Location, p2: Location):
-    """ stub """
+def calc_flight_time(
+        p1: Location,
+        p2: Location
+) -> float:
+    """
+    find out how much time it will take to get from `p1` to `p2`
+
+    Args:
+        p1 (Location): departure
+        p2 (Location): destination
+    """
     speed_factor = 1.0  # element  [0, 1]
     minimum_speed = 100
     universe_fleet_speed = 7
@@ -46,12 +55,31 @@ def calc_flight_time(p1: Location, p2: Location):
 
 
 class OrePrice:
+    """
+    represents cost and or price
+
+    TODO: consider implement energy, too.
+          some research construction needs one off energy
+    """
     metal: Union[float, int] = None
     crystal: Union[float, int] = None
     deuterium: Union[float, int] = None
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+    def __str__(self):
+        return f'M:{self.metal:.0f} C:{self.crystal:.0f} D:{self.deuterium:.0f}'
+
+    def __repr__(self):
+        return f'M:{self.metal:.1E} C:{self.crystal:.1E} D:{self.deuterium:.1E}'
+
+    def __eq__(self, other):
+        return all([
+            self.metal == other.metal,
+            self.crystal == other.crystal,
+            self.deuterium == other.deuterium
+        ])
 
     def __and__(self, other):
         return OrePrice(
@@ -60,12 +88,12 @@ class OrePrice:
             deuterium=self.deuterium + other.deuterium
         )
 
-    def to_dict(self):
-        return {
-            'metal': self.metal,
-            'crystal': self.crystal,
-            'deuterium': self.deuterium
-        }
+    def __add__(self, other):
+        return OrePrice(
+            metal=self.metal + other.metal,
+            crystal=self.crystal + other.crystal,
+            deuterium=self.deuterium + other.deuterium
+        )
 
     def __sub__(self, other):
         return OrePrice(
@@ -88,26 +116,21 @@ class OrePrice:
             deuterium=self.deuterium ** factor
         )
 
+    def to_dict(self):
+        return {
+            'metal': self.metal,
+            'crystal': self.crystal,
+            'deuterium': self.deuterium
+        }
+
     def get_meu(self, rate: ExchangeRate = None):
+        raise NotImplementedError()
         if rate is None:
             rate = ExchangeRate(metal=3, crystal=2, deuterium=1)
         base = rate.metal + rate.crystal + rate.deuterium
         meu = (
-                (rate.metal / base) * self.metal
-                + (rate.crystal / base) * self.crystal
-                + (rate.deuterium / base) * self.deuterium
+                  (1 / rate.metal) * self.metal
+                + (1 / rate.crystal) * self.crystal
+                + (1 / rate.deuterium) * self.deuterium
         )
         return meu
-
-    def __eq__(self, other):
-        return all([
-            self.metal == other.metal,
-            self.crystal == other.crystal,
-            self.deuterium == other.deuterium
-        ])
-
-    def __str__(self):
-        return f'M:{self.metal:.0f} C:{self.crystal:.0f} D:{self.deuterium:.0f}'
-
-    def __repr__(self):
-        return f'M:{self.metal:.1E} C:{self.crystal:.1E} D:{self.deuterium:.1E}'

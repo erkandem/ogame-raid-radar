2631fb2cfef9fe5f390e58ff1d309c79aea53a4d
kan <erkan.dem@pm.me>
2020-04-22 16:38:24 +0200

    add topic string

--- src/api/setup.py
+++ src/api/setup.py
@@ -27,10 +27,8 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Games/Entertainment :: Simulation',
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
